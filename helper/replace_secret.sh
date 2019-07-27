ce84a26a0172be2fbd258780ba6915a952664b1e
Artin <lengthmin@gmail.com>
2019-07-27 13:03:19 +0800

    add travis ci

---
+++ helper/replace_secret.sh
@@ -0,0 +1,9 @@
+cp _config_base.yml _config.yml
+sed -i -e "s/_leancloud_counter_security_username_/$leancloud_counter_security_username/g" _config.yml
+sed -i -e "s/_leancloud_counter_security_password_/$leancloud_counter_security_password/g" _config.yml
+sed -i -e "s/_leancloud_app_id_/$leancloud_app_id/g" _config.yml
+sed -i -e "s/_leancloud_app_key_/$leancloud_app_key/g" _config.yml
+sed -i -e "s/_gitalk_client_id_/$gitalk_client_id/g" _config.yml
+sed -i -e "s/_gitalk_client_secret_/$gitalk_client_secret/g" _config.yml
+sed -i -e "s/_GH_TOKEN_/${GH_TOKEN}/g" _config.yml
+sed -i -e "s/_CODING_TOKEN_/${CODING_TOKEN}/g" _config.yml
