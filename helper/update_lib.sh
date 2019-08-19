c7651a4cf4582c141661c1a95bf608fc764e6141
Artin <lengthmin@gmail.com>
2019-08-19 22:11:46 +0800

    Update Next

--- helper/update_lib.sh
+++ helper/update_lib.sh
@@ -1,6 +1,3 @@
 git -C themes/next pull || git clone https://github.com/theme-next/hexo-theme-next.git themes/next
-git -C themes/next/source/lib/fastclick pull ||  git clone https://github.com/theme-next/theme-next-fastclick.git themes/next/source/lib/fastclick
 git -C themes/next/source/lib/pace pull || git clone https://github.com/theme-next/theme-next-pace.git themes/next/source/lib/pace
-git -C themes/next/source/lib/lazyload pull || git clone https://github.com/theme-next/theme-next-lazyload.git themes/next/source/lib/lazyload
-git -C themes/next/source/lib/quicklink pull || git clone https://github.com/theme-next/theme-next-quicklink.git themes/next/source/lib/quicklink
-git -C themes/next/source/lib/reading_progress pull || git clone https://github.com/theme-next/theme-next-reading-progress themes/next/source/lib/reading_progress
+git -C themes/next/source/lib/pjax pull || git clone https://github.com/theme-next/theme-next-pjax themes/next/source/lib/pjax
