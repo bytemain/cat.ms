37e91d2a046d1aed6347c7e8100f119d37f2bbbe
Artin <lengthmin@gmail.com>
2019-12-28 22:28:44 +0800

    chore: update config

--- scripts/hook-copy-right.js
+++ scripts/hook-copy-right.js
@@ -1,16 +1,15 @@
 /* global hexo */
 
-"use strict"
+"use strict";
 
 hexo.extend.filter.register(
   "theme_inject",
   function(injects) {
     injects.postBodyEnd.file(
       "creative-commons",
-      "source/_data/copyright/post-copyright.njk",
-      hexo.locals
-    )
-    injects.style.push("source/_data/copyright/post-copyright.styl")
+      "source/_data/copyright/post-copyright.njk"
+    );
+    injects.style.push("source/_data/copyright/post-copyright.styl");
   },
   100
-)
+);
