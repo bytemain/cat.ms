9775e2c4804440b76522119c3e0e8ffcb4854865
lengthmin <lengthmin@gmail.com>
2021-04-22 14:28:02 +0800

    fix generate friends

--- helper/new.js
+++ helper/new.js
@@ -31,16 +31,20 @@ if (!typeArray.includes(type)) {
 
 console.log(`type:`, type);
 console.log(`title:`, title);
-let exec;
-if (type !== "page") {
-  if (title[0] >= "0" && title[0] <= "9") {
-    prefix = prefix + "-";
-  }
-  let slug = `${prefix}${title}`;
-  exec = execa(`yarn hexo new ${type} ${title} -s ${slug}`);
-} else {
-  exec = execa(`yarn hexo new page ${title}`);
-}
 
-exec.stdout.pipe(transformFactory()).pipe(process.stdout);
-exec.stderr.pipe(transformFactory()).pipe(process.stderr);
+try {
+  let exec;
+  if (type !== "page") {
+    if (title[0] >= "0" && title[0] <= "9") {
+      prefix = prefix + "-";
+    }
+    let slug = `${prefix}${title}`;
+    exec = execa("yarn", ["hexo", "new", type, title, "-s", slug]);
+  } else {
+    exec = execa("yarn", ["hexo", "new", "page", title]);
+  }
+  exec.stdout.pipe(transformFactory()).pipe(process.stdout);
+  exec.stderr.pipe(transformFactory()).pipe(process.stderr);
+} catch (error) {
+  console.log(error);
+}
--- scripts/generate-friends.js
+++ scripts/generate-friends.js
@@ -3,27 +3,31 @@ const fs = require("fs");
 const path = require("path");
 const nunjucks = require("nunjucks");
 
-hexo.extend.tag.register("friends", function (args) {
-  const friends = yaml.safeLoad(
-    fs.readFileSync(path.join(__dirname, "../data/friends.yml"), "utf8")
-  );
-  let array = [];
-  Object.keys(friends).forEach((v) => {
-    array.push({
-      name: v,
-      href: friends[v].href,
-      intro: friends[v].intro,
+hexo.extend.tag.register(
+  "friends",
+  function (args) {
+    const friends = yaml.load(
+      fs.readFileSync(path.join(__dirname, "../data/friends.yml"), "utf8")
+    );
+    let array = [];
+    Object.keys(friends).forEach((v) => {
+      array.push({
+        name: v,
+        href: friends[v].href,
+        intro: friends[v].intro,
+      });
     });
-  });
-  let template = `<ul>
+    let template = `<ul>
   {% for item in items -%}
     <li>
-      <a class="name" href="{{ item.href }}" target="_blank">{{ item.name }}</a>
+      <a class="name" href="{{item.href}}" target="_blank">{{item.name}}</a>
       <br>
-      <span class="intro">{{ item.intro }}</span>
+      <span class="intro">{{item.intro}}</span>
     </li>
   {%- endfor %}
   </ul>`;
-  let result = nunjucks.renderString(template, { items: array });
-  return result;
-});
+    let result = nunjucks.renderString(template, { items: array });
+    return result;
+  },
+  { async: true }
+);
