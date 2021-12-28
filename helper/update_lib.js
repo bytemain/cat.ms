90ba711834684a78558f926e292fb6a8812856e5
Artin <lengthmin@gmail.com>
2021-12-28 10:09:49 +0000

    update dependencies

--- helper/update_lib.js
+++
@@ -1,9 +0,0 @@
-const { transformFactory } = require("./utils.js");
-const execa = require("execa");
-const exec = execa(
-  "git -C themes/next pull --rebase || git clone https://github.com/next-theme/hexo-theme-next.git themes/next",
-  { shell: true }
-);
-
-exec.stdout.pipe(transformFactory()).pipe(process.stdout);
-exec.stderr.pipe(transformFactory()).pipe(process.stderr);
